72a8c09b8a83c6d8286f96f0789bb38b1a65a9cc
Jisan <jisanfuerte@jisan-macbook.local>
2025-04-14 16:43:45 +0530

    og tag support

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    trailingSlash: true,
-    assetPrefix: '/',
-    images: {
-        domains: ["firebasestorage.googleapis.com", "encrypted-tbn0.gstatic.com","media.istockphoto.com","lh3.googleusercontent.com","www.medicoverhospitals.in","cdn.hexahealth.com"],
-        unoptimized: true,  
-      },
-    
+ 
+  trailingSlash: true,
+  assetPrefix: '/',
+  images: {
+      domains: ["firebasestorage.googleapis.com", "encrypted-tbn0.gstatic.com","media.istockphoto.com","lh3.googleusercontent.com","www.medicoverhospitals.in","cdn.hexahealth.com"],
+      unoptimized: true,  
+    },
+  
 };
 
 export default nextConfig;
