028fe2f978714233088ef571531e14428fe210b9
Jisan <jisanfuerte@jisan-macbook.local>
2025-04-22 16:01:15 +0530

    testing tags

--- app/cataract/page.js
+++ app/cataract/page.js
@@ -6,6 +6,12 @@ export const metadata  = {
   title: 'Cataract Surgery – Trusted Solution for Clear Vision at the Right Price',
   description: 'Experience world-class cataract surgery at an affordable price. Trust Aapka Care for safe, effective, and permanent vision correction.',
   keywords: 'Cataract Surgery',
+  openGraph: {
+  title: 'Cataract Surgery – Trusted Solution for Clear Vision at the Right Price',
+  description: 'Experience world-class cataract surgery at an affordable price. Trust Aapka Care for safe, effective, and permanent vision correction.',
+  url: 'https://aapkacare.com/cataract', // optional but helpful for SEO
+  type: 'website',
+  },
   
 
 }
