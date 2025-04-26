c2c0340a773b5afe49a570f83c9b2021ee2fd227
Jisan <jisanfuerte@jisan-macbook.local>
2025-04-26 10:57:41 +0530

    updated code

--- app/privacyPolicy/page.js
+++ app/privacyPolicy/page.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function PrivacyPolicy() {
+const PrivacyPage = () => {
   return (
     <div className="max-w-4xl mx-auto px-4 py-10 text-gray-800">
       <h1 className="text-4xl font-bold mb-6 text-center">Fuerte Health Care Private Limited Privacy Policy</h1>
@@ -112,3 +112,5 @@ export default function PrivacyPolicy() {
     </div>
   );
 }
+
+export default PrivacyPage
