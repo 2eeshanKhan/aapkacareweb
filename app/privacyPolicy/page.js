33163be68ad92090745557e68ab46b78620ddec3
Jisan <jisanfuerte@jisan-macbook.local>
2025-04-26 11:12:47 +0530

    new changes

--- app/privacyPolicy/page.js
+++ app/privacyPolicy/page.js
@@ -4,31 +4,7 @@ const PrivacyPage = () => {
   return (
     <div className="max-w-4xl mx-auto px-4 py-10 text-gray-800">
       <h1 className="text-4xl font-bold mb-6 text-center">Fuerte Health Care Private Limited Privacy Policy</h1>
-      <div className="space-y-6 text-justify">
-        <p>
-          Fuerte Health Care Pvt Ltd ("Aapka Care"), a company incorporated in India, having its corporate office at 8th Floor, Spaze Platinum Tower, Sector-47, Gurgaon-122001.
-        </p>
-
-        <p>
-          Aapka Care is the owner of{" "}
-          <a href="https://www.aapkacare.com" className="text-blue-600 underline">
-            https://www.aapkacare.com
-          </a>{" "}
-          and the Aapka Care mobile application (the URLs and mobile application together referred to as "Websites" and individually a "Website"). The terms 'You' or 'Your' refer to you as the user (whether you are a Registered User or Unregistered User) of the Website or of the services offered by the Website ("Service").
-        </p>
-
-        <p>
-          Aapka Care and Fuerte Health Care Pvt Ltd are committed to your privacy. This privacy policy ("Privacy Policy") applies to the collection, use, storage, processing, disclosure, and transfer of your information when you use the Website and/or the Services.
-        </p>
-
-       
-        <p>
-          You may unsubscribe from our communications or update your preferences by contacting us at{" "}
-          <a href="mailto:contact@aapkacare.com" className="text-blue-600 underline">
-           contact@aapkacare.com
-          </a>.
-        </p>
-      </div>
+      
     </div>
   );
 }
