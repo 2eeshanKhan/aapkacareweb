514533ad3a07410b07c3faf6fbb2306836cac9ee
Jisan <jisanfuerte@jisan-macbook.local>
2025-04-28 10:49:29 +0530

    new tag manager

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientLayout from '@/components/Client/ClientLayout'
-import { GoogleTagManager } from '@next/third-parties/google'
+import Script from 'next/script'
+
 
 
 
@@ -38,12 +39,34 @@ export default function RootLayout({ children }) {
   
   return (
     <html lang="en">
-    <GoogleTagManager gtmId="GTM-N26W8RH8" />
+    
 
       
    
 
     <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-XXXXXXX');
+            `,
+          }}
+        />
+
+<noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-N26W8RH8`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
    
     <ClientLayout>{children}</ClientLayout>
 
