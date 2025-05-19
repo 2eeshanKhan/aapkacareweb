5bd3eb3dea53618be7eb3a240e2e244900e0a76f
Jisan <jisanfuerte@jisan-macbook.local>
2025-05-19 15:13:24 +0530

    appendix page added

--- components/Client/ClientLayout.jsx
+++ components/Client/ClientLayout.jsx
@@ -5,7 +5,7 @@ import Header from '@/components/Layouts';
 
 export default function ClientLayout({ children }) {
   const pathname = usePathname();
-  const hideHeaderPaths = ['/lasik/book-consultancy', '/lasik/book-consultancy/','/kidney-stones/book-consultancy','/kidney-stones/book-consultancy/','/thank-you','/thank-you/','/lipoma/book-consultancy/','/lipoma/book-consultancy','/hernia/book-consultancy','/hernia/book-consultancy/'];
+  const hideHeaderPaths = ['/lasik/book-consultancy', '/lasik/book-consultancy/','/kidney-stones/book-consultancy','/kidney-stones/book-consultancy/','/thank-you','/thank-you/','/lipoma/book-consultancy/','/lipoma/book-consultancy','/hernia/book-consultancy','/hernia/book-consultancy/','/appendix/book-consultancy','/appendix/book-consultancy/'];
 
   const showHeader = !hideHeaderPaths.includes(pathname);
 
